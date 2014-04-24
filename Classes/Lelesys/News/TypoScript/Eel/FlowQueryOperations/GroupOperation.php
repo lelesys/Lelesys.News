a26e2b1a2bf35f36f469f87e5db39e672abd02fa
Pankaj Lele <pankaj@lelesys.com>
2014-04-24 13:56:51 +0530

    [TASK] Several bugfixes and feature additions
    Bugfixes
    --------
    * Pagination is not working
    * CategoryMenu content element not working
    * Page reload is missing at some places when something is changed in the inspector
    
    Features / Improvements
    -----------------------
    * Validations (inspector) - added validations wherever possible for the properties edited in inspector
    * Archive menu - new node type is now available which renders the year and montwise archive menu
    * News tags - Comma separated tags can be added to news item and then filtering of news list by a tag is possible
    * RSS feed - Add .rss extension to any News Folder node and you can see the RSS feed of that folder
    * Meta tags - Keyword and description meta tags are rendered on news single pages

---
+++ Classes/Lelesys/News/TypoScript/Eel/FlowQueryOperations/GroupOperation.php
@@ -0,0 +1,68 @@
+<?php
+namespace Lelesys\News\TypoScript\Eel\FlowQueryOperations;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Lelesys.News".          *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Eel\FlowQuery\Operations\AbstractOperation;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use TYPO3\Eel\FlowQuery\FlowQuery;
+
+/**
+ * Group news nodes collection
+ * TODO - this can be optimized a little further to make it work on grouping of
+ * any nodes and not only menu items
+ */
+class GroupOperation extends  AbstractOperation {
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var string
+	 */
+	static protected $shortName = 'group';
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var integer
+	 */
+	static protected $priority = 100;
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * We can only handle Menu items for now.
+	 *
+	 * @param mixed $context
+	 * @return boolean
+	 */
+	public function canEvaluate($context) {
+		return (isset($context[0]['node']) && ($context[0]['node'] instanceof NodeInterface));
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param FlowQuery $flowQuery the FlowQuery object
+	 * @param array $arguments the arguments for this operation
+	 * @return mixed
+	 */
+	public function evaluate(FlowQuery $flowQuery, array $arguments) {
+		$items = $flowQuery->getContext();
+		$groupedItems = array();
+		foreach ($items as $item) {
+			if (isset($item['node'])) {
+				$dateTime = $item['node']->getProperty('dateTime');
+				if ($dateTime instanceof \DateTime) {
+					$year = $dateTime->format('Y');
+					$month = $dateTime->format('m');
+					$groupedItems[$year][$month][] = $item['node'];
+				}
+			}
+		}
+		$flowQuery->setContext($groupedItems);
+	}
+}
---
+++ Classes/Lelesys/News/TypoScript/Eel/FlowQueryOperations/PropertyOperation.php
@@ -0,0 +1,77 @@
+<?php
+namespace Lelesys\News\TypoScript\Eel\FlowQueryOperations;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Lelesys.News".          *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Eel\FlowQuery\Operations\AbstractOperation;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use TYPO3\Eel\FlowQuery\FlowQuery;
+
+/**
+ * Extended EEL property() operation for News
+ */
+class PropertyOperation extends AbstractOperation {
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var string
+	 */
+	static protected $shortName = 'property_array';
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var integer
+	 */
+	static protected $priority = 100;
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var boolean
+	 */
+	static protected $final = TRUE;
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * We can only handle TYPO3CR Nodes.
+	 *
+	 * @param mixed $context
+	 * @return boolean
+	 */
+	public function canEvaluate($context) {
+		return (isset($context[0]) && ($context[0] instanceof NodeInterface) && $context[0]->getNodeType()->isOfType('Lelesys.News:List'));
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param FlowQuery $flowQuery the FlowQuery object
+	 * @param array $arguments the arguments for this operation
+	 * @return mixed
+	 */
+	public function evaluate(FlowQuery $flowQuery, array $arguments) {
+		if (!isset($arguments[0]) || empty($arguments[0])) {
+			throw new \TYPO3\Eel\FlowQuery\FlowQueryException('property_array() does not support returning all attributes yet', 1332492263);
+		} else {
+			$context = $flowQuery->getContext();
+			$propertyPath = $arguments[0];
+
+			if (!isset($context[0])) {
+				return NULL;
+			}
+
+			$element = $context[0];
+			if ($propertyPath[0] === '_') {
+				return \TYPO3\Flow\Reflection\ObjectAccess::getPropertyPath($element, substr($propertyPath, 1));
+			} else {
+				return $element->getProperty($propertyPath, TRUE);
+			}
+		}
+	}
+}
